3aa4361bfbed9f13eebf6d830651be834672c21b
Zach Aysan <zachaysan@gmail.com>
2013-05-15 10:43:10 -0400

    fix comment to update new reality

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,9 +1,6 @@
 // by default, persist application data to localStorage change this file
 // to use the RESTAdapter or your own adapter.
 
-
-// Disable data storage for now, so that we can extend it later
-
 App.Adapter = DS.RESTAdapter.extend({
   bulkCommit: false
 });
