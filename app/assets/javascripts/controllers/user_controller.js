9e4b0cec4eb3e07b0380dc1432151c4c05285dbb
Zach Aysan <zachaysan@gmail.com>
2013-05-15 14:40:35 -0400

    and of course stop editing

--- app/assets/javascripts/controllers/user_controller.js
+++ app/assets/javascripts/controllers/user_controller.js
@@ -7,5 +7,11 @@ App.UserController = Ember.ObjectController.extend({
     userEditController.set('content', this.get('content'));
     userEditController.startEditing();
     this.set('isEditing', true);
+  },
+  
+  stopEditing: function(){
+    var contactEditController = this.get('controllers.userEdit');
+    contactEditController.stopEditing();
+    this.set('isEditing', false);
   }
 });
--- app/assets/javascripts/controllers/user_edit_controller.js
+++ app/assets/javascripts/controllers/user_edit_controller.js
@@ -1,12 +1,19 @@
 App.UserEditController = Ember.ObjectController.extend({
-  needs: ['contact'],
+  needs: ['user'],
 
-  startEditing: function() {
+  startEditing: function(){
     var user = this.get('content');
     var transaction = user.get('store').transaction();
 
     transaction.add(user);
     this.transaction = transaction;
+  },
 
+  stopEditing: function(){
+    var transaction = this.transaction;
+    if (transaction){
+      transaction.rollback();
+      this.transaction = undefined;
+    }
   }
 });
