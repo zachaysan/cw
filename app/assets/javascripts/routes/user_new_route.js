300cf3c1d15199ee04b4cb099d9fae839a8517ae
Zach Aysan <zachaysan@gmail.com>
2013-05-16 16:16:05 -0400

    new user route

--- app/assets/javascripts/routes/user_new_route.js
+++ app/assets/javascripts/routes/user_new_route.js
@@ -1,17 +1,15 @@
 App.UserNewRoute = Ember.Route.extend({
   model: function() {
     // the record gets created in the controller
-    console.log("woah nelly!!!!");
     return null;
   },
 
   setupController: function(controller) {
-    console.log("about to setup");
     controller.startEditing();
   },
   
   deactivate: function(controller) {
-    this.controllerFor('users.new').stopEditing();
+    this.controllerFor('user.new').stopEditing();
   }
 
 });
