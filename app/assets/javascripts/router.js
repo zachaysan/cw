e4442e18b3b957e6e2e3fa296e693465766f126e
Zach Aysan <zachaysan@gmail.com>
2013-05-23 17:07:14 -0400

    add access token to router, still have some questions though.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,12 @@
 App.Router.map(function() {
   this.resource("user", function(){
     this.route("new");
+    // Not sure if I should put the access token
+    // resource in here
   });
+  // or to keep it as a separate resource here
+  this.resource("access_token", function(){
+    // Need to figure out how to set this to /login
+    this.route("new");
+  }
 });
