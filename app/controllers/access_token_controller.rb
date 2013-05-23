3d3b771b013a13a00fef005ad2e15d396b43e33b
Zach Aysan <zachaysan@gmail.com>
2013-05-23 16:14:32 -0400

    create or find by user auth

---
+++ app/controllers/access_token_controller.rb
@@ -0,0 +1,23 @@
+class AccessTokenController < ApplicationController
+
+  respond_to :json
+
+  def create
+    user = User.authenticate(user_auth_params)
+    if not user
+      respond_with(:error => "Password is wrong or email (user) doesn't exist", :status => :unauthorized)
+    else
+      access_token = AccessToken.find_or_create_by_user(user)
+      access_token.update_attribute(:updated_at, Time.now)
+      respond_with(access_token,
+                   :location => access_tokens_url)
+    end
+  end
+
+  private
+  
+  def user_auth_params
+    params.require(:user_auth).requre(:email,
+                                      :password)
+  end
+end
--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -2,4 +2,10 @@ class AccessToken < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :token
+
+  before_save :generate_token
+
+  def generate_token
+    self.token = SecureRandom.hex
+  end
 end
