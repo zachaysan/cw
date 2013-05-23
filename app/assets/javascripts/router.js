188e6a276655eec294506cc3c78d7c581398a9a0
Zach Aysan <zachaysan@gmail.com>
2013-05-23 16:48:05 -0400

    remove login and password reset

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,6 +2,4 @@ App.Router.map(function() {
   this.resource("user", function(){
     this.route("new");
   });
-  this.route("login", { path: "/login" });
-  this.route("resetPassword", { path: "/forgot_password" });
 });
