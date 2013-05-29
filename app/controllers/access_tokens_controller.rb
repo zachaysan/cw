9dc965e33277d1d4829b7b5e4e7c2ef4d9272cb7
Zach Aysan <zachaysan@gmail.com>
2013-05-29 18:57:00 -0400

    create access token controller

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -3,11 +3,13 @@ class AccessTokensController < ApplicationController
   respond_to :json
 
   def create
-    user = User.authenticate(user_auth_params)
+    user = User.authenticate(access_token_params)
     if not user
-      respond_with(:error => "Password is wrong or email (user) doesn't exist", :status => :unauthorized)
+      respond_with(:error => "Password is wrong or email (user) doesn't exist",
+                   :status => :unauthorized)
     else
       access_token = AccessToken.find_or_create_by_user(user)
+      puts access_token
       access_token.update_attribute(:updated_at, Time.now)
       respond_with(access_token,
                    :location => access_tokens_url)
@@ -16,8 +18,8 @@ class AccessTokensController < ApplicationController
 
   private
   
-  def user_auth_params
-    params.require(:user_auth).requre(:email,
-                                      :password)
+  def access_token_params
+    params.require(:access_token).permit(:email,
+                                         :password)
   end
 end
