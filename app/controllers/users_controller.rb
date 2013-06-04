653a9c7f3e3c67eac0fe73a276713329096802ef
Zach Aysan <zachaysan@gmail.com>
2013-06-04 10:07:46 -0400

    respond with location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  # TODO: Auth
 
   respond_to :json
 
   def create
     user = User.create(user_params)
-    respond_with(user, :location => users_url)
+    respond_with(user, :location => user)
   end
 
   private
