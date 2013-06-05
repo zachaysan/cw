95cf5d5164dc0c1eecac9ca32a05ad9f600399bc
Zach Aysan <zachaysan@gmail.com>
2013-06-05 09:27:18 -0400

    Add comments

--- app/assets/javascripts/models/access_token.js
+++ app/assets/javascripts/models/access_token.js
@@ -1,4 +1,7 @@
 App.AccessToken  = DS.Model.extend({
   email:        DS.attr('string'),
   password:     DS.attr('string')
+  
+  // After the post response updates the model 
+  // we must add the AccessToken.token attr.
 });
