b47321e6c6275e7facc5c9ccc67ed67da58e2825
Zach Aysan <zachaysan@gmail.com>
2013-06-18 17:06:50 -0400

    Create producers controller

---
+++ app/controllers/producers_controller.rb
@@ -0,0 +1,17 @@
+class ProducersController < ApplicationController
+
+  respond_to :json
+
+  def create
+    producer = Producer.create(producer_params)
+    respond_with(producer, :location => producer)
+  end
+
+  private
+
+  def producer_params
+    params.require(:producer).permit(:name,
+                                     :owner_id,
+                                     :user_ids)
+  end
+end
