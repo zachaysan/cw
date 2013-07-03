b54e7c325e0f33af6bcfa7b471713013cba344be
Zach Aysan <zachaysan@gmail.com>
2013-07-03 17:44:06 -0400

    Remove stale comments

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,14 +1,12 @@
-// by default, persist application data to localStorage change this file
-// to use the RESTAdapter or your own adapter.
+Ember.$.ajaxSetup({
+  headers: { 'AUTHORIZATION': localStorage.access_token }
+});
 
 App.Adapter = DS.RESTAdapter.extend({
   bulkCommit: false
 });
 
-// TODO: auto-add the headers
-
 App.Store = DS.Store.extend({
   revision: 12,
   adapter:  App.Adapter.create()
 });
-
