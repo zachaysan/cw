0784fa598d6502a3fc73a750a9de9e0eea74ff39
Zach Aysan <zachaysan@gmail.com>
2013-07-09 11:50:47 -0400

    Remove stubbed attrs

--- app/assets/javascripts/views/producer_in_list_view.js
+++ app/assets/javascripts/views/producer_in_list_view.js
@@ -1,9 +1,4 @@
 App.ProducerInListView = Em.View.extend({
   templateName: 'producer_in_list',
-  tagName: 'li',
-  classNameBindings: 'isActive:active',
-
-  isActive: function() {
-    return this.get('content.id') === this.get('controller.activeProducerId');
-  }.property('controller.activeProducerId')
+  tagName: 'li'
 });
