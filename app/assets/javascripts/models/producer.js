8a8b22755cc9b1a74b73e21f461659ee52a1d6b4
Zach Aysan <zachaysan@gmail.com>
2013-07-09 11:44:30 -0400

    Add name and stats to producer model

--- app/assets/javascripts/models/producer.js
+++ app/assets/javascripts/models/producer.js
@@ -1,3 +1,5 @@
 App.Producer = DS.Model.extend({
-  name:        DS.attr('string'),
+  name:           DS.attr('string'),
+  webhook_count:  DS.attr('string'),
+  consumer_count: DS.attr('string'),
 });
