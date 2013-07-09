f9e6252c70655cde2a5f0b53689b97bdc292660c
Zach Aysan <zachaysan@gmail.com>
2013-07-09 11:39:05 -0400

    Add debugging functions

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -15,3 +15,9 @@ Ember.LOG_BINDINGS = SUPER_DEBUG;
 App = Ember.Application.create({LOG_TRANSITIONS: SUPER_DEBUG});
 
 App.deferReadiness();
+var debug_routes = function(){
+  return Ember.keys(App.Router.router.recognizer.names);
+}
+var debug_templates = function(){
+  return Ember.keys(Ember.TEMPLATES);
+}
