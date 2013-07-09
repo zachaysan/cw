0a2431a5c490ba6455e459f9f93681173df40391
Zach Aysan <zachaysan@gmail.com>
2013-07-09 16:02:46 -0400

    Introduce the consumers to the producer model

--- app/assets/javascripts/models/producer.js
+++ app/assets/javascripts/models/producer.js
@@ -2,4 +2,5 @@ App.Producer = DS.Model.extend({
   name:           DS.attr('string'),
   webhook_count:  DS.attr('string'),
   consumer_count: DS.attr('string'),
+  consumers:      DS.hasMany('App.Consumer'),
 });
