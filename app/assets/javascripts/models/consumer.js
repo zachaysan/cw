5153e44664f0b995c41be42bd504ce07b4f8e8e7
Zach Aysan <zachaysan@gmail.com>
2013-07-10 16:59:05 -0400

    Add webhooks to consumer

--- app/assets/javascripts/models/consumer.js
+++ app/assets/javascripts/models/consumer.js
@@ -1,5 +1,7 @@
 App.Consumer = DS.Model.extend({
-  name:           DS.attr('string'),
-  webhook_count:  DS.attr('string'),
-  producer:       DS.belongsTo('App.Producer'),
+  name:             DS.attr('string'),
+  webhook_count:    DS.attr('string'),
+  producer:         DS.belongsTo('App.Producer'),
+  webhooks:         DS.hasMany('App.Webhook'),
+  failed_webhooks:  DS.hasMany('App.Webhook'),
 });
