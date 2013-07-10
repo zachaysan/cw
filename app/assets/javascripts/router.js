0c8eabea0cb67ce8ffc573bb8909a6cd88ad7743
Zach Aysan <zachaysan@gmail.com>
2013-07-10 10:20:52 -0400

    Add consumer to router

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,7 @@
 App.Router.map(function() {
   this.resource("producers", function(){});
   this.resource("producer", { path: 'producer/:producer_id' });
+  this.resource("consumer", { path: 'consumer/:consumer_id' });
   this.resource("user", function(){
     this.route("new");
   });
