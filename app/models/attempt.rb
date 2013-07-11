c9c625c6249073d1c6a6e6fa99954e2eec46ea35
Zach Aysan <zachaysan@gmail.com>
2013-07-11 09:04:42 -0400

    Add register_error code to attempt

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,4 +1,11 @@
 class Attempt < ActiveRecord::Base
   belongs_to :webhook
+  belongs_to :error
   validates_presence_of :success
+
+  def register_error(error)
+    error = Error.where(message: error.message).any? || nil
+    error ||= Error.new(message: error.message)
+    error.save!
+  end
 end
