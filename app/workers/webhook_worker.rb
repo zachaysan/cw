85959d25feadabcd20f899e7e720a39a71e867f9
Zach Aysan <zachaysan@gmail.com>
2013-07-11 11:10:21 -0400

    Handle attempts and errors in the webhook worker

--- app/workers/webhook_worker.rb
+++ app/workers/webhook_worker.rb
@@ -7,12 +7,26 @@ class WebhookWorker
 
   def perform(webhook_id)
     webhook = Webhook.find(webhook_id)
-    resp = self.class.post(webhook.post_uri,
-                           body: webhook.post_data)
-
-    webhook.attempt = false
-    webhook.save!
+    attempt = Attempt.new
+    attempt.webhook = webhook
+    begin
+      resp = self.class.post(webhook.post_uri,
+                             body: webhook.post_data)
+      webhook.save!
+      attempt.success = true
+    rescue Exception => e
+      puts e.message
+      attempt.success = false
+      err = (PostError.where(message: e.message)).any? || nil
+      err ||= PostError.new
+      err.message = e.message
+      attempt.error = err
+      attempt.save
+    end
+    webhook.attempt = false if not webhook.attempt
+    webhook.save
   end
+
 end
 
 module Clockwork
