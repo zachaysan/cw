7892dfa5a435482da35bdf5bce233bcecf59fd45
Zach Aysan <zachaysan@gmail.com>
2013-07-13 13:32:37 -0400

    Add created_at to webhook model

--- app/assets/javascripts/models/webhook.js
+++ app/assets/javascripts/models/webhook.js
@@ -3,5 +3,6 @@ App.Webhook = DS.Model.extend({
   post_data:  DS.attr('string'),
   attempt:    DS.attr('boolean'),
   failed:     DS.attr('boolean'),
+  created_at: DS.attr('string'),
   consumer:  DS.belongsTo('App.Consumer'),
 });
