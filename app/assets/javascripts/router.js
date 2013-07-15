4652a89d2e29b57fb029b3f5ae0df8a4b1f15529
Zach Aysan <zachaysan@gmail.com>
2013-07-15 15:32:15 -0400

    Nest webhook to consumer

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,8 +1,9 @@
 App.Router.map(function() {
   this.resource("producers", function(){});
   this.resource("producer", { path: 'producer/:producer_id' });
-  this.resource("consumer", { path: 'consumer/:consumer_id' });
-  this.resource("webhook", { path: 'webhook/:webhook_id' });
+  this.resource("consumer", { path: 'consumer/:consumer_id'}, function(){
+    this.resource("webhook", { path: 'webhook/:webhook_id' });
+  });
   this.resource("user", function(){
     this.route("new");
   });
