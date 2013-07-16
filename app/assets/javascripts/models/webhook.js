19186a49724c63dec57bac5663bf74214d5b06dd
Zach Aysan <zachaysan@gmail.com>
2013-07-16 15:22:15 -0400

    Update webhook model capitalization

--- app/assets/javascripts/models/webhook.js
+++ app/assets/javascripts/models/webhook.js
@@ -1,8 +1,8 @@
 App.Webhook = DS.Model.extend({
-  post_uri:   DS.attr('string'),
-  post_data:  DS.attr('string'),
+  postUri:   DS.attr('string'),
+  postData:  DS.attr('string'),
   attempt:    DS.attr('boolean'),
   failed:     DS.attr('boolean'),
-  created_at: DS.attr('string'),
+  createdAt: DS.attr('string'),
   consumer:  DS.belongsTo('App.Consumer'),
 });
