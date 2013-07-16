259bc24ad22b3e8fdff591f52ac8b9e695fb9db5
Zach Aysan <zachaysan@gmail.com>
2013-07-16 15:21:41 -0400

    Update consumer capitalization

--- app/assets/javascripts/models/consumer.js
+++ app/assets/javascripts/models/consumer.js
@@ -1,7 +1,7 @@
 App.Consumer = DS.Model.extend({
   name:             DS.attr('string'),
-  webhook_count:    DS.attr('string'),
+  webhookCount:    DS.attr('string'),
   producer:         DS.belongsTo('App.Producer'),
   webhooks:         DS.hasMany('App.Webhook'),
-  failed_webhooks:  DS.hasMany('App.Webhook'),
+  failedWebhooks:  DS.hasMany('App.Webhook'),
 });
