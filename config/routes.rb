63748d4936e091f3971fe11a30407d2b442ad941
Zach Aysan <zachaysan@gmail.com>
2013-07-18 15:45:07 -0400

    Create the non-ember page for the marketing site

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Cw::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'application#index'
+  root :to => 'welcomes#index'
 
   mount Sidekiq::Web => '/sidekiq'
 
